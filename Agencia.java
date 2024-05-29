155560ca029ff4f4c1a24dfb7516bc3692733900
Lunes313 <99741938+Lunes313@users.noreply.github.com>
2024-05-28 21:46:52 -0500

    update atributos

---
+++ Agencia.java
@@ -0,0 +1,21 @@
+public class Agencia {
+
+    public static void main(String[] args) {
+
+        menu();
+    }
+
+    private static void menu() {
+        System.out.println("Bienvenido a la agencia de viajes");
+        System.out.println("1. Registrarse");
+        System.out.println("2. Iniciar sesión");
+        System.out.println("3. reservar");
+        System.out.println("4. cancelar reserva");
+        System.out.println("5. Mostrar reservas");
+        System.out.println("6. Mostrar destinos");
+        System.out.println("9. Salir");
+
+    }
+
+}
+
